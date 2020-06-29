4d5b5ee8e8cd780f5a61fbe62639bead9138a829
Chandrakiran K S <chandrakiran.ks@gmail.com>
2020-06-29 12:50:39 +0530

    added ios new version script
    added ios new version script

---
+++ bin/fkiosapp-get-next-release-version.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+preReleaseNumber=$1
+
+major=$(echo $preReleaseNumber | sed 's/^\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)(\([0-9]*\))$/\1/g')
+minor=$(echo $preReleaseNumber | sed 's/^\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)(\([0-9]*\))$/\2/g')
+patch=$(echo $preReleaseNumber | sed 's/^\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)(\([0-9]*\))$/\3/g')
+buildNo=$(echo $preReleaseNumber | sed 's/^\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)(\([0-9]*\))$/\4/g')
+
+case $2 in
+patch)
+  buildNo=$((buildNo + 1))
+  printf "$major.$minor.$patch($buildNo)"
+  ;;
+hotfix)
+  patch=$((patch + 1))
+  buildNo=1
+  printf "$major.$minor.$patch($buildNo)"
+  ;;
+minor)
+  patch=1
+  buildNo=1
+  minor=$((minor + 1))
+  printf "$major.$minor.$patch($buildNo)"
+  ;;
+major)
+  patch=1
+  minor=1
+  buildNo=1
+  major=$(($major + 1))
+  printf "$major.$minor.$patch($buildNo)"
+  ;;
+esac
